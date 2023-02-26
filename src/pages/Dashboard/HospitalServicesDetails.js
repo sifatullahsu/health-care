4651e47324bc0592eb1da91792e417a9b93be5ff
sifatullahsu <personal.sifat@gmail.com>
2023-02-27 05:11:27 +0600

    dynamically slot handle

--- src/pages/Dashboard/HospitalServicesDetails.js
+++ src/pages/Dashboard/HospitalServicesDetails.js
@@ -2,24 +2,47 @@ import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 
 const HospitalServicesDetails = () => {
   const id = useLoaderData();
+  const navigate = useNavigate();
 
   const date = new Date();
   const [selectedDate, setSelectedDate] = useState(date);
   const formatedDate = format(selectedDate, 'PP');
 
   const { data: service = [], isLoading } = useQuery({
-    queryKey: ['service'],
+    queryKey: ['service', formatedDate],
     queryFn: async () => {
-      const res = await fetch(`http://localhost:5000/api/v1/services/single/${id}`);
+      const res = await fetch(`http://localhost:5000/api/v1/services/single/${id}?date=${formatedDate}`);
       const data = await res.json();
 
       return data?.status ? data.data : [];
     }
   });
+
+
+  const handleCheckout = (event, slotRadio, doctor) => {
+    event.preventDefault();
+
+    const slot = event.target[slotRadio].value;
+
+    const stateData = {
+      appointment: {
+        date: formatedDate,
+        slot: slot
+      },
+      service: {
+        _id: service._id,
+        name: service.name,
+        price: service.price
+      },
+      doctor: { ...doctor }
+    }
+
+    navigate('/dashboard/checkout', { state: stateData })
+  }
 
 
   const footer = selectedDate ? <p>You selected {format(selectedDate, 'PP')}.</p> : <p>Please pick a day.</p>;
@@ -47,49 +70,55 @@ const HospitalServicesDetails = () => {
           service?.doctors.map((doctor, i) => {
             return (
               <div key={i} className='card bg-white shadow-xl relative'>
-                <div className='card-body justify-between'>
-                  <div>
-                    <p className='text-sm text-accent absolute top-5 right-5'>{doctor?.slots.length} spaces available</p>
-                    <div key={doctor?._id} className='flex gap-3 min-h-[5rem]'>
-                      <img src={doctor?.image} className='basis-14 h-12 mt-1' alt="" />
-                      <div className='basis-full'>
-                        <h3 className="text-lg text-secondary font-bold capitalize">{doctor?.name}</h3>
-                        <p className='text-sm text-accent text-ellipsis'>{`${doctor?.designation} - [${doctor.qualifications}]`}</p>
+                <form onSubmit={(event) => handleCheckout(event, `radio_${i}`, {
+                  _id: doctor._id,
+                  name: doctor.name,
+                  email: doctor.email,
+                  qualifications: doctor.qualifications,
+                  designation: doctor.designation,
+                  image: doctor.image
+                })}>
+                  <div className='card-body justify-between'>
+                    <div>
+                      <p className='text-sm text-accent absolute top-5 right-5'>{doctor?.slots.length} spaces available</p>
+                      <div key={doctor?._id} className='flex gap-3 min-h-[5rem]'>
+                        <img src={doctor?.image} className='basis-14 h-12 mt-1' alt="" />
+                        <div className='basis-full'>
+                          <h3 className="text-lg text-secondary font-bold capitalize">{doctor?.name}</h3>
+                          <p className='text-sm text-accent text-ellipsis'>{`${doctor?.designation} - [${doctor.qualifications}]`}</p>
+                        </div>
+                      </div>
+                      <div className='text-accent'>
+                        <p className='text-sm'>{doctor?.about.length > 160 ? <>{`${doctor.about.slice(0, 160)}...`}</> : doctor?.about}</p>
+                        <div className='my-3'>
+                          {
+                            doctor.slots.map((slot, index) => {
+                              return (
+                                <span key={index} className='inline-block'>
+                                  <label
+                                    className="label cursor-pointer text-xs py-0 pl-[6px] pr-2 rounded-sm mr-2"
+                                    style={{ background: "rgb(208 208 208)" }}
+                                  >
+                                    <input
+                                      type="radio"
+                                      name={`radio_${i}`}
+                                      value={slot}
+                                      className="radio radio-xs checked:bg-red-500 mr-1"
+                                    />
+                                    <span className="label-text">{slot}</span>
+                                  </label>
+                                </span>
+                              );
+                            })
+                          }
+                        </div>
                       </div>
                     </div>
-                    <div className='text-accent'>
-                      <p className='text-sm'>{doctor?.about.length > 160 ? <>{`${doctor.about.slice(0, 160)}...`}</> : doctor?.about}</p>
-                      <div className='my-3'>
-                        {doctor.slots.map(i => <span key={i} className='text-xs px-1 rounded-sm inline-block mr-2' style={{ background: "rgb(208 208 208)" }}>{i}</span>)}
-                      </div>
+                    <div>
+                      <button className='btn btn-primary btn-sm text-xs min-h-[25px] h-[25px]'>Book Now</button>
                     </div>
                   </div>
-                  <div>
-                    <Link
-                      to='/dashboard/checkout'
-                      className='btn btn-primary btn-sm text-xs min-h-[25px] h-[25px]'
-                      state={{
-                        appointment: {
-                          date: formatedDate,
-                          slot: '11.30 AM - 12.00 AM'
-                        },
-                        service: {
-                          _id: service._id,
-                          name: service.name,
-                          price: service.price
-                        },
-                        doctor: {
-                          _id: doctor._id,
-                          name: doctor.name,
-                          email: doctor.email,
-                          qualifications: doctor.qualifications,
-                          designation: doctor.designation,
-                          image: doctor.image
-                        }
-                      }}
-                    >Book Now</Link>
-                  </div>
-                </div>
+                </form>
               </div>
             );
           })
