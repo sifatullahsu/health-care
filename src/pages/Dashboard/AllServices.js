9bfbbccde3b62fc01219118eb37ec5534ec86fc6
sifatullahsu <personal.sifat@gmail.com>
2023-03-09 08:51:34 +0600

    service page skeleton effect

--- src/pages/Dashboard/AllServices.js
+++ src/pages/Dashboard/AllServices.js
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { FaRegEdit } from "react-icons/fa";
+import Skeleton from 'react-loading-skeleton';
 import { Link } from 'react-router-dom';
 import Pagination from '../../components/Pagination';
 import Thead from '../../components/Thead';
@@ -33,18 +34,36 @@ const AllServices = () => {
 
           <tbody>
             {
-              services?.data?.map((service, index) =>
-                <tr key={index}>
-                  <th>{index + 1}</th>
-                  <td>{service.name}</td>
-                  <td>${service.price}</td>
-                  <td className='text-right'>
-                    <Link to={`/dashboard/services/${service._id}`}>
-                      <FaRegEdit className='inline'></FaRegEdit>
-                    </Link>
-                  </td>
-                </tr>
-              )
+              !isLoading ?
+                <>
+                  {
+                    services?.data?.map((service, index) =>
+                      <tr key={index}>
+                        <th>{index + 1}</th>
+                        <td>{service.name}</td>
+                        <td>${service.price}</td>
+                        <td className='text-right'>
+                          <Link to={`/dashboard/services/${service._id}`}>
+                            <FaRegEdit className='inline'></FaRegEdit>
+                          </Link>
+                        </td>
+                      </tr>
+                    )
+                  }
+                </>
+                :
+                <>
+                  {
+                    Array(10).fill('').map((_, i) =>
+                      <tr key={i}>
+                        <th><Skeleton /></th>
+                        <td><Skeleton /></td>
+                        <td><Skeleton /></td>
+                        <td><Skeleton /></td>
+                      </tr>
+                    )
+                  }
+                </>
             }
           </tbody>
 
